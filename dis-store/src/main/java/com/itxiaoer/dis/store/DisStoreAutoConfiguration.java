2c4a5999313b3453e030aee82ac0c341b854c7e4
liuyk <271007729@qq.com>
2018-12-29 02:50:04 +0800

    修改不启用加载redis的问题

--- dis-store/src/main/java/com/itxiaoer/dis/store/DisStoreAutoConfiguration.java
+++ dis-store/src/main/java/com/itxiaoer/dis/store/DisStoreAutoConfiguration.java
@@ -1,15 +1,18 @@
 package com.itxiaoer.dis.store;
 
+import com.itxiaoer.dis.store.redis.DisRedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author : liuyk
  */
 @ComponentScan
 @Configuration
+@Import(DisRedisAutoConfiguration.class)
 @EnableConfigurationProperties(DisStoreProperties.class)
 @ConditionalOnProperty(value = "spring.dis.active", havingValue = "true")
 public class DisStoreAutoConfiguration {
